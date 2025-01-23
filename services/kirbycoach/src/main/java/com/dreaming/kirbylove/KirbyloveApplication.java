ed742c5efefc28f2eaa9b2621c6db89eb7403f31
dreamzzing <mint0962@naver.com>
2025-01-23 15:05:37 +0900

    init

---
+++ services/kirbycoach/src/main/java/com/dreaming/kirbylove/KirbyloveApplication.java
@@ -0,0 +1,13 @@
+package com.dreaming.kirbylove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KirbyloveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KirbyloveApplication.class, args);
+	}
+
+}
---
+++ services/kirbycoach/src/test/java/com/dreaming/kirbylove/KirbyloveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreaming.kirbylove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KirbyloveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
